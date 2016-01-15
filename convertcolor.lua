1aa81666f012cba3f6a7095f3f4afe5479eeae71
SpycerLviv <spycer.lviv@gmail.com>
2016-01-16 01:21:37 +0200

    shorten functions

--- convertcolor.lua
+++ convertcolor.lua
@@ -1,19 +1,19 @@
 local M = {}
 
-local function hexColor (hex) 
+local function hex (hex) 
 	local redColor,greenColor,blueColor=hex:match('(..)(..)(..)')
 	redColor, greenColor, blueColor = tonumber(redColor, 16)/255, tonumber(greenColor, 16)/255, tonumber(blueColor, 16)/255
 	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
 	return (redColor..', '..greenColor..', '..blueColor)
 end
 
-local function rgbColor (r, g, b)
+local function rgb (r, g, b)
 	local redColor,greenColor,blueColor=r/255, g/255, b/255
 	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
 	return (redColor..', '..greenColor..', '..blueColor)
 end
 
-M.hexColor = hexColor
-M.rgbColor = rgbColor
+M.hex = hex
+M.rgb = rgb
 
 return M
