8129e2684e4127a9f081070262bb012a181f01c8
SpycerLviv <spycer.lviv@gmail.com>
2016-01-18 12:44:00 +0200

    Change 'return's from string to values

--- convertcolor.lua
+++ convertcolor.lua
@@ -4,13 +4,13 @@ local function hex (hex)
 	local redColor,greenColor,blueColor=hex:match('(..)(..)(..)')
 	redColor, greenColor, blueColor = tonumber(redColor, 16)/255, tonumber(greenColor, 16)/255, tonumber(blueColor, 16)/255
 	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
-	return (redColor..', '..greenColor..', '..blueColor)
+	return redColor, greenColor, blueColor
 end
 
 local function rgb (r, g, b)
 	local redColor,greenColor,blueColor=r/255, g/255, b/255
 	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
-	return (redColor..', '..greenColor..', '..blueColor)
+	return redColor, greenColor, blueColor
 end
 
 M.hex = hex
