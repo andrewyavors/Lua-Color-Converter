61e3835db569a4942946838fedf43af21b32753c
Fab <fabianjarrett@gmail.com>
2023-08-17 15:28:41 +0100

    Remove rounding colours
    Rounding the colours is inaccurate and returning a colour that is noticeable.

--- convertcolor.lua
+++ convertcolor.lua
@@ -3,7 +3,6 @@ local M = {}
 local function hex (hex, alpha) 
 	local redColor,greenColor,blueColor=hex:match('#?(..)(..)(..)')
 	redColor, greenColor, blueColor = tonumber(redColor, 16)/255, tonumber(greenColor, 16)/255, tonumber(blueColor, 16)/255
-	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
 	if alpha == nil then
 		return redColor, greenColor, blueColor
 	end
@@ -12,13 +11,11 @@ end
 
 local function rgb (r, g, b)
 	local redColor,greenColor,blueColor=r/255, g/255, b/255
-	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
 	return redColor, greenColor, blueColor
 end
 
 local function rgba (r, g, b, alpha)
 	local redColor,greenColor,blueColor=r/255, g/255, b/255
-	redColor, greenColor, blueColor = math.floor(redColor*100)/100, math.floor(greenColor*100)/100, math.floor(blueColor*100)/100
 	return redColor, greenColor, blueColor, alpha
 end
 
